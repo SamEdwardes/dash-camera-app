54102cba86640cd7075fd64d7eda8e2caccf7c3c
SamEdwardes <edwardes.s@gmail.com>
2021-09-06 22:24:41 -0700

    Added boilerplate and simple working code.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dash-camera-app
+
 A Dash app that can access your camera.
+
+## Install
+
+Using pip:
+
+```bash
+pip install -r requirements.txt
+```
+
+Using poetry:
+
+```bash
+poetry install
+```
+
+## Run the app
+
+```bash
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,33 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Upload(id='upload', children=html.Button('Upload an image')),
+    html.Img(id='image', width=500)
+])
+
+
+# Preview image or video after taking without sending to the server. This is
+# helpful because the app is much slower if you send content to the server
+# first.
+app.clientside_callback(
+    """
+    function(contents) {
+        // document.write(contents)
+        if (contents === undefined) {
+            return "";
+        } else {
+            return contents;
+        }
+    }
+    """,
+    Output('image', 'src'),
+    Input('upload', 'contents')
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
